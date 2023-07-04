d951a0c1c59ab7268d7006e2c3c054404ec5f8f0
Lilith Whittles <lwhittles@users.noreply.github.com>
2023-07-04 15:39:00 +0100

    GB linelist

--- src/data_covid_cases/script.R
+++ src/data_covid_cases/script.R
@@ -8,7 +8,7 @@ source("support.R")
 
 ## Instructions: save your API key in an untracked file named 'key.txt'
 key <- readLines("key.txt")
-country <- "NZ" ## using NZ for now as rate-limited for larger data sets
+country <- "GB" ## using NZ for now as rate-limited for larger data sets
 
 linelist <- get_cases(apikey = key, country = country)
 cases <- get_cases_GdotH_API(linelist)
