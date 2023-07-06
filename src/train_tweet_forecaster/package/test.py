9a17e48455e16c7d487835cc6f3a188addd1d561
Giovanni Charles <giovanni.charles@gmail.com>
2023-07-06 12:38:40 +0100

    Training loop

--- src/train_tweet_forecaster/package/test.py
+++ src/train_tweet_forecaster/package/test.py
@@ -1,13 +1,23 @@
-from .train import get_train_state
+from .train import get_train_state, train
 from jax.random import PRNGKey
 from jax import numpy as jnp
+import jax
 
 if __name__ == '__main__':
-    state = get_train_state(PRNGKey(0), 7)
+    # test that I can make a train state
+    lag = 7
+    state = get_train_state(PRNGKey(0), 10, lag)
     print(
         state.apply_fn(
             state.params,
             jnp.ones((1, 100, 10)),
-            jnp.ones((1, 8, 2))
+            jnp.ones((1, lag + 1, 2))
         ).shape
     )
+
+    # test that I can run a training loop
+    X = jax.random.uniform(PRNGKey(1), (100, 100, 10))
+    y_train = jax.random.uniform(PRNGKey(2), (100, 100, 1))
+    y_test = jax.random.uniform(PRNGKey(2), (100, lag, 1))
+
+    train(PRNGKey(3), X, y_train, y_test, epochs=5, batch_size=10)
--- src/train_tweet_forecaster/package/train.py
+++ src/train_tweet_forecaster/package/train.py
@@ -6,19 +6,20 @@ from jaxtyping import Array
 from flax.training import train_state
 import jax
 import jax.numpy as jnp
+from jax import random
 import optax
 from .rnn import Seq2seq
 
-def get_train_state(rng: Any, lag: int) -> train_state.TrainState:
+def get_train_state(rng: Any, n_features: int, lag: int) -> train_state.TrainState:
     """Returns a train state."""
     model = Seq2seq(
         teacher_force = True,
-        hidden_size = 10,
+        hidden_size = 128,
         eos_id = -1
     )
     params = model.init(
         rng,
-        jnp.ones((1, 100, 10)),
+        jnp.ones((1, 100, n_features)),
         jnp.ones((1, lag + 1, 2))
     )
     tx = optax.adam(1e-2)
@@ -29,12 +30,12 @@ def get_train_state(rng: Any, lag: int) -> train_state.TrainState:
     )
     return state
 
-def nll_loss(y_hat: Array, y: Array) -> float:
+def nll_loss(y_hat: Array, y: Array) -> Array:
     """Returns gauss negative log likelihood."""
     mu, sigma = y_hat[0,:], y_hat[1,:]
-    return jnp.log(sigma) / 2 + jnp.square(y - mu) / (2 * sigma)
+    return jnp.sum(jnp.log(sigma) / 2 + jnp.square(y - mu) / (2 * sigma))
 
-def compute_metrics(y_hat: Array, y: Array) -> Dict[str, float]:
+def compute_metrics(y_hat: Array, y: Array) -> Dict[str, Array]:
     """Computes metrics and returns them."""
     loss = nll_loss(y_hat, y)
     mse = jnp.mean(jnp.square(y - y_hat))
@@ -48,33 +49,65 @@ def compute_metrics(y_hat: Array, y: Array) -> Dict[str, float]:
 @jax.jit
 def train_step(
         state: train_state.TrainState,
-        batch: Array,
-        eos_id: int
-    ) -> Tuple[train_state.TrainState, Dict[str, float]]:
+        X: Array,
+        y_input: Array,
+        y_test: Array
+    ) -> Tuple[train_state.TrainState, Dict[str, Array]]:
     """Trains one step."""
-    y = batch['answer'][:, 1:]
 
     def loss_fn(params):
         y_hat = state.apply_fn(
-                params,
-                batch['query'],
-                batch['answer']
-                )
-        loss = nll_loss(y_hat, y)
+            params,
+            X,
+            y_input
+        )
+        loss = nll_loss(y_hat, y_test)
         return loss, y_hat
 
     grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
     (_, y_hat), grads = grad_fn(state.params)
     state = state.apply_gradients(grads=grads)
-    metrics = compute_metrics(y_hat, y)
+    metrics = compute_metrics(y_hat, y_test)
 
     return state, metrics
 
-def train(key: Any, X: Array, y: Array, epochs: int = 100):
-    lag = y.shape[1]
-    state = get_train_state(key, lag)
+def train(
+    key: Any,
+    X: Array,
+    y_train: Array,
+    y_test: Array,
+    epochs: int = 100,
+    batch_size = 100
+    ):
+    # get the lag
+    lag = y_test.shape[1]
+
+    # Create input sequences 
+    X_input = jnp.concatenate([X, y_train], axis=-1)
+
+    # Create output sequences 
+    y_input = jnp.concatenate([y_train[:,-1:,:], y_test], axis=1)
+    y_std = jnp.ones_like(y_input)
+    y_input = jnp.concatenate([y_input, y_std], axis=-1)
+
+    # Batch the sequences
+    X_batched = jnp.split(X_input, batch_size)
+    y_input_batched = jnp.split(y_input, batch_size)
+    y_test_batched = jnp.split(y_test, batch_size)
+
+    # Ceate the train state
+    key, key_i = random.split(key)
+    state = get_train_state(key_i, X_input.shape[-1], lag)
+
+    # Train
     for step in range(epochs):
-        #TODO: minibatching
-        state, metrics = train_step(state, X, y)
+        key, key_i = random.split(key)
+        for b in random.permutation(key_i, len(X_batched)):
+            state, metrics = train_step(
+                state,
+                X_batched[b],
+                y_input_batched[b],
+                y_test_batched[b]
+            )
         print(metrics)
     return state
