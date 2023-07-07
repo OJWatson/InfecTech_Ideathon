55562b70b9f619ed2aae6f1ef924c53fc5438437
Bimandra Djaafara <bimandra.djaafara@gmail.com>
2023-07-07 12:02:58 +0100

    add arima with tweet as exogenous variables

--- src/time_series_analysis_forecast/script.R
+++ src/time_series_analysis_forecast/script.R
@@ -4,6 +4,7 @@ library(feasts)
 library(urca)
 library(dplyr)
 library(tidyverse)
+library(cowplot)
 source("time_series_analysis_forecast/support.R")
 
 ## The output of this script is .csv files of forecast and
@@ -12,6 +13,20 @@ source("time_series_analysis_forecast/support.R")
 
 ## Read GB Covid-19 time series of cases data from global.health
 data <- read_csv("data_covid_cases/outputs/cases_GB.csv")
+
+## Read processed tweets data from tweet_analysis folder
+tweet_data <- read_csv("tweet_analysis/tweets_and_topics.csv")
+
+## Summarise tweet data - daily
+tweet_daily_data <- tweet_data %>% 
+  mutate(date = as.Date(date)) %>% 
+  group_by(date) %>% 
+  summarise(topic_0 = sum(topic_0), topic_1 = sum(topic_1),
+            topic_2 = sum(topic_2), topic_3 = sum(topic_3),
+            topic_4 = sum(topic_4), topic_5 = sum(topic_5),
+            topic_6 = sum(topic_6), topic_7 = sum(topic_7),
+            topic_8 = sum(topic_8), topic_9 = sum(topic_9)) %>% 
+  filter(date <= max(ymd(data$date)))
 
 ## Time series model: ETS and ARIMA
 ## Forecast starting in March 2020
@@ -32,7 +47,7 @@ for (i in seq_along(forecast_date)){
                                            method = 'ets',
                                            date_start = start_date,
                                            date_end = training_end_date,
-                                           length = "14 days", ci = 95) %>% 
+                                           len = "14 days",ci=95) %>% 
     mutate(forecast_date=forecast_date[i])
 }
 fc_ets <- as_tibble(bind_rows(fc_ets_list))
@@ -45,28 +60,43 @@ for (i in seq_along(forecast_date)){
                                              method = 'arima',
                                              date_start = start_date,
                                              date_end = training_end_date,
-                                             length = "14 days", ci = 95) %>% 
+                                             len = "14 days", ci = 95) %>% 
     mutate(forecast_date=forecast_date[i])
 }
 fc_arima <- as_tibble(bind_rows(fc_arima_list))
+
+## Forecast using ARIMA with tweets data
+fc_arima_tweet_list <- list()
+for (i in seq_along(forecast_date)){
+  training_end_date <- forecast_date[i] - 1
+  fc_arima_tweet_list[[i]] <- time_series_tweets_forecast(data = data,
+                                                          tweet_data = tweet_daily_data,
+                                                          method = 'arima',
+                                                          date_start = start_date,
+                                                          date_end = training_end_date,
+                                                          len = "14 days", ci = 95) %>% 
+    mutate(forecast_date=forecast_date[i])
+}
+fc_arima_tweet <- as_tibble(bind_rows(fc_arima_tweet_list))
 
 ## Output
 ## .csv output
 write_csv(fc_ets,"time_series_analysis_forecast/outputs/output_fc_ets.csv")
 write_csv(fc_arima,"time_series_analysis_forecast/outputs/output_fc_arima.csv")
+write_csv(fc_arima_tweet,"time_series_analysis_forecast/outputs/output_fc_arima_tweet.csv")
 
 ## Combine data and all forecast
 output <- data %>% mutate(model="data") %>% 
   rename(x = cases) %>% 
-  bind_rows(fc_ets,fc_arima) %>% 
-  mutate(model = factor(model, levels = c("arima","ets","data")))
+  bind_rows(fc_ets,fc_arima,fc_arima_tweet) %>% 
+  mutate(model = factor(model, levels = c("arima","ets","arima_tweet","data")))
 
 fc_plot <- output %>% ggplot() +
-  geom_ribbon(aes(x = date, ymin = lower, ymax = upper, 
+  geom_ribbon(aes(x = date, ymin = lower, ymax = upper,
                   col = model, fill = model)) +
   geom_point(aes(x = date, y = x, col = model, fill = model)) +
   geom_line(aes(x = date, y = mean, 
-                col = model, fill = model, group = forecast_date)) +
+                col = model, group = forecast_date)) +
   scale_y_log10() +
   theme_bw() +
   theme(legend.position = "bottom") +
@@ -78,8 +108,9 @@ ggsave("time_series_analysis_forecast/outputs/fc_over_time.png")
 output_last_fc <- output %>% 
   filter(model == "data" | forecast_date == forecast_date[length(forecast_date)])
 
-last_fc_plot <- output_last_fc %>% ggplot() +
-  geom_ribbon(aes(x = date, ymin = lower, ymax = upper, 
+last_fc_plot <- output_last_fc %>% tail(100) %>% 
+  ggplot() +
+  geom_ribbon(aes(x = date, ymin = lower, ymax = upper,
                   col = model, fill = model)) +
   geom_point(aes(x = date, y = x, col = model, fill = model)) +
   geom_line(aes(x = date, y = mean, 
--- src/time_series_analysis_forecast/support.R
+++ src/time_series_analysis_forecast/support.R
@@ -5,11 +5,13 @@
 ## date_start: starting date for training data
 ## date_end: ending date for training data
 ## --note: if either date_start or date_end is NULL, then use entire data for training
-## length: length of forecast
-## ci: confidence interval in %
+## len: length of forecast
+## ci: confidence interval in % - currently hardcoded to 95
 
 time_series_forecast <- function(data,method='ets',date_start=NULL,date_end=NULL,
-                                 length="14 days",ci=95){
+                                 len="14 days",ci=95){
+  ci <- 95
+  
   # rename columns of data
   colnames(data) <- c("date","x")
   data$date <- ymd(data$date)
@@ -39,9 +41,110 @@ time_series_forecast <- function(data,method='ets',date_start=NULL,date_end=NULL
   
   # make forecast from time series model
   fc <- fit %>%
-    forecast(h = length) %>%
+    forecast(h = len) %>%
     hilo(level = ci) %>% 
-    unpack_hilo(cols = paste0(ci,'%')) %>% 
+    # unpack_hilo(cols = as.name(paste0(ci,'%'))) %>%
+    unpack_hilo(`95%`) %>% # currently hardcoded
+    dplyr::select(model=.model,date,mean=.mean,
+                  lower=paste0(ci,'%_lower'),upper=paste0(ci,'%_upper')) %>%
+    mutate(ci_level=ci)
+  
+  return(fc)
+}
+
+## Forecasting using time series analysis methods - adding tweets data
+## data: data going to be used, should be formatted: 1st column: date, 2nd column: data to be forecast
+## --note: date should be in the ymd format, e.g., "2020-12-25"
+## tweet_data: daily summary of tweets data
+## method: choose 'arima'
+## date_start: starting date for training data
+## date_end: ending date for training data
+## --note: if either date_start or date_end is NULL, then use entire data for training
+## len: length of forecast
+## ci: confidence interval in % - currently hardcoded to 95
+
+time_series_tweets_forecast <- function(data,tweet_data,method='arima',date_start=NULL,date_end=NULL,
+                                        len="14 days",ci=95){
+  ci <- 95
+  
+  # rename columns of data
+  colnames(data) <- c("date","x")
+  data$date <- ymd(data$date)
+  tweet_data$date <- ymd(tweet_data$date)
+  tweet_data <- tweet_data %>% replace(is.na(.), 0)
+  
+  # combine cases and tweets data
+  data <- data %>% left_join(tweet_data) %>% replace(is.na(.), 0)
+  
+  # subset data for training
+  if (is.null(date_start) | is.null(date_end)){
+    date_start <- ymd(min(data$date))
+    date_end <- ymd(max(data$date))
+  }
+  data_training <- tibble(data.frame(date=seq(date_start,date_end,"day"))) %>% 
+    left_join(data) %>% tsibble
+  
+  # find optimal lags for each of the tweets topic variables: max of 14 days
+  n_col_tweets <- ncol(tweet_data) - 1
+  optimal_lag <- list()
+  for (i in seq_len(n_col_tweets)){
+    x <- data_training[,2]
+    y <- data_training[,2+i]
+    optimal_lag[[i]] <- which.max(sapply(1:14, 
+                                         function(j) cor(y, lag(x, j), use = "complete")))
+  }
+  
+  # replace covariates with lagged covariates for training data
+  data_training_lag <- data_training
+  for (i in seq_len(n_col_tweets)){
+    data_training_lag[,2+i] <- lag(data_training[,2+i],n = optimal_lag[[i]])
+  }
+  
+  data_training_lag <- data_training_lag %>% replace(is.na(.), 0)
+  
+  # create n-days ahead of covariates for forecast
+  dur_forecast_days <- round(as.numeric(as.duration(len))/86400)
+  forecast_window <- seq(date_end+1,date_end+dur_forecast_days,"days")
+  
+  # forecast window outside current data
+  forecast_window_outside <- forecast_window[forecast_window>max(data$date)]
+  
+  # data for forecast: for forecast create additional data for n-days ahead
+  if (length(forecast_window_outside) > 0){
+    data_add_for_forecast <- as_tibble(data.frame(date=forecast_window_outside,
+                                                  matrix(0,nrow=length(forecast_window_outside),
+                                                         ncol=ncol(data)-1)))
+    colnames(data_add_for_forecast) <- colnames(data)
+    data_add_for_forecast <- data_add_for_forecast %>% 
+      mutate(date=ymd(date)) %>% tsibble
+    data_for_forecast <- bind_rows(data,data_add_for_forecast)
+  } else {
+    data_for_forecast <- data
+  }
+  
+  data_lag <- data_for_forecast
+  for (i in seq_len(n_col_tweets)){
+    data_lag[,2+i] <- lag(data_for_forecast[,2+i],optimal_lag[[i]])
+  }
+  data_forecast_lag <- data_lag %>% filter(date %in% forecast_window) %>% tsibble
+  
+  # train time series model
+  if (method=='arima'){
+    fit <- data_training_lag %>%
+      model(
+        arima_tweet = ARIMA(log(x + 1) ~ topic_0 + topic_1 + topic_2 + topic_3 + topic_4 + 
+                              topic_5 + topic_6 + topic_7 + topic_8 + topic_9)
+      )
+  } else {
+    break('Please specify method as arima')
+  }
+  
+  # make forecast from time series model
+  fc <- fit %>%
+    forecast(data_forecast_lag) %>%
+    hilo(level = ci) %>% 
+    # unpack_hilo(cols = as.name(paste0(ci,'%'))) %>%
+    unpack_hilo(`95%`) %>% # currently hardcoded
     dplyr::select(model=.model,date,mean=.mean,
                   lower=paste0(ci,'%_lower'),upper=paste0(ci,'%_upper')) %>% 
     mutate(ci_level=ci)
